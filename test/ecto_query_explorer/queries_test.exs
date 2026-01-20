b628ea6d845dc53bed1930ad8ec435829ff71a66
Ievgen Pyrogov <iamexile@gmail.com>
2026-01-20 13:04:45 +0100

    Run Migration1 in tests for epochs support

--- test/ecto_query_explorer/queries_test.exs
+++ test/ecto_query_explorer/queries_test.exs
@@ -41,7 +41,7 @@ defmodule EctoQueryExplorer.QueriesTest do
     :ok = Ecto.Adapters.SQL.Sandbox.mode(Repo, :manual)
     :ok = Ecto.Adapters.SQL.Sandbox.checkout(Repo)
 
-    Ecto.Migrator.run(Repo, [{0, EctoQueryExplorer.Migration0}], :up,
+    Ecto.Migrator.run(Repo, [{0, EctoQueryExplorer.Migration0}, {1, EctoQueryExplorer.Migration1}], :up,
       all: true,
       log_migrations_sql: :debug
     )
